70a9c84edc068e6dd7d9f3f5f4f188321067539a
Bridget Shannon <bridgetrshannon@gmail.com>
2020-11-16 19:17:58 -0500

    created functions to go with api routes

---
+++ create-react-express/routes/api/google.js
@@ -0,0 +1,7 @@
+/api/books (get) - Should return all saved books as JSON.
+
+/api/books (post) - Will be used to save a new book to the database.
+
+/api/books/:id (delete) - Will be used to delete a book from the database by Mongo _id.
+
+* (get) - Will load your single HTML page in client/build/index.html. Make sure you have this after all other routes are defined.
