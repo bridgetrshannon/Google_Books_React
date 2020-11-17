08fc0feeed43faf38f66324ff272361ed94ee964
Bridget Shannon <bridgetrshannon@gmail.com>
2020-11-16 21:50:43 -0500

    added content to index.js file in routes

--- create-react-express/controllers/googleController.js
+++ create-react-express/controllers/googleController.js
@@ -4,5 +4,26 @@ const db = require("../models");
 module.exports = {
   findAll: function (req, res) {
     // api call goes here
-  },
+    const { query: params } = req;
+    axios.get("https://www.googleapis.com/books/v1/volumes", {
+      params
+    });
+    .then(results =>
+        results.data.items.filter(
+            result =>
+            result.volumeInfo.title &&
+            result.volumeInfo.infoLink &&
+            result.volumeInfo.authors &&
+            result.volumeInfo.description &&
+            result.volumeInfo.imageLinks &&
+            result.volumeInfo.imageLinks.thumbnail
+        )
+        )
+        .then(apiBooks =>
+            db.Book.find().then(dbBooks =>
+                apiBooks.filter(apiBook =>
+                    dbBooks.every(dbBook => dbBook.googleId.toString() != apiBook.id)
+                    )
+                ))
+  }
 };
